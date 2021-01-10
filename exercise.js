a8aba3e7b3fc68b26925453034bf92b72082a9cf
Calvin Roberts <roberts.calvin85@gmail.com>
2021-01-10 22:58:59 +0000

    JQuery Exercise.

---
+++ exercise.js
@@ -0,0 +1,11 @@
+// Select all divs & give them a purple background 
+$("div").css("background", "purple");
+
+// Select the divs with "highlight" & make the 200px wide
+$(".highlight").css("width", "200px");
+
+// Select the div with id "third" & give it a orange border
+$("#third").css("border", "4px solid orange");
+
+// Bonus: Select the first div only & change it's font color to pink
+$("div:first-of-type").css("color", "pink");
